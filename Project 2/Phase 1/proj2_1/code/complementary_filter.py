8a9a598810fbeabcaa2eea71d712d89dd0286b58
“IKRunner” <iachilihu@gmail.com>
2021-03-29 03:54:05 -0400

    Update complementary_filter.py

--- Project 2/Phase 1/proj2_1/code/complementary_filter.py
+++ Project 2/Phase 1/proj2_1/code/complementary_filter.py
@@ -21,6 +21,6 @@ def complementary_filter_update(initial_rotation, angular_velocity, linear_accel
     # TODO Your code here - replace the return value with one you compute
     t=1
     r=2
-    y=9
+    y=9433
 
     return Rotation.identity()
