362bbcca3eeecf49290d96649272764cab065850
“IKRunner” <iachilihu@gmail.com>
2021-03-29 03:00:27 -0400

    Add projectt

---
+++ Project 2/Phase 1/proj2_1/setup.py
@@ -0,0 +1,12 @@
+#%%
+from distutils.core import setup
+
+#%%
+setup(
+    name='aerial_robotics',
+    version='0.1',
+    install_requires=[
+            'pyyaml',
+            'matplotlib',
+            'numpy',
+            'scipy'])
