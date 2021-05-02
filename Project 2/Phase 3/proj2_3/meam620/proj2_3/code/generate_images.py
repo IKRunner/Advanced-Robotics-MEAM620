f11e02ced369725dc96fae4a39878c9e7479f8aa
IKRunner <iachilihu@gmail.com>
2021-05-02 12:38:59 -0400

    update

---
+++ Project 2/Phase 3/proj2_3/meam620/proj2_3/code/generate_images.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+image_folder = '../dataset/MachineHall01_reduced/cam0/data'
+
+video_name = '../data_out/video.avi'
+
+
+
+
+
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
